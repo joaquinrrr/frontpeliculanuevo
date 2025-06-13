a34f7d4dac494179f944a214898008c4aaf97ef5
Educando23 <134257028+Eduardox21@users.noreply.github.com>
2025-06-13 17:36:25 -0500

    commoit 222

--- src/app/components/home/secondhome/secondhome.ts
+++ src/app/components/home/secondhome/secondhome.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../../services/login.service';
 
 @Component({
   selector: 'app-secondhome',
@@ -6,6 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './secondhome.html',
   styleUrl: './secondhome.css'
 })
-export class Secondhome {
+export class Secondhome implements OnInit {
+  username: string = '';
+  currentTime: string = '';
+  whatsappLink: string = 'https://wa.me/967775359';
+  whatsappLinkAdmin: string = 'https://wa.me/967775359';
 
+  constructor(private loginService: LoginService) {}
+  ngOnInit(): void {
+    this.updateTime();
+    this.username = this.loginService.showUsername();
+    setInterval(() => {
+      this.updateTime();
+    }, 1000); // Actualiza la hora cada segundo
+  }
+
+  updateTime(): void {
+    const now = new Date();
+    this.currentTime = now.toLocaleTimeString('es-PE', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+  }
 }
